c188935647627702fd757a88fa153871fa6797d2
deepak saini <70664666+itsDeku@users.noreply.github.com>
2023-10-06 13:42:52 +0530

    Updated ourService.js

--- src/component/ourService/ourService.js
+++ src/component/ourService/ourService.js
@@ -1,74 +1,55 @@
-import React , { useEffect }from 'react'
+import React, { useEffect } from 'react'
 import "./ourService.css"
 
 const OurService = () => {
-  useEffect(() => {
-    const slider = document.querySelector('.slider');
-
-    const handleAnimationIteration = () => {
-      // Reset the animation when it completes one cycle
-      slider.style.animation = 'none';
-      setTimeout(() => {
-        slider.style.animation = 'slide 10s linear infinite';
-      });
-    };
-
-    slider.addEventListener('animationiteration', handleAnimationIteration);
-
-    return () => {
-      slider.removeEventListener('animationiteration', handleAnimationIteration);
-    };
-  }, []);
 
   return (
     <>
-    <div className='ourService'> 
+      <div className='ourService'>
         <div className='serviceContent'>
-            <h4>OUR SERVICES</h4>
-            <h1>We Offer a Wide Range Of Service</h1>
+          <h4>OUR SERVICES</h4>
+          <h1>We Offer a Wide Range Of Service</h1>
         </div>
-        <div className='serviceList slider-container'>
-        <div className=' abc serviceFirst slider'>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            {/* <!-- Duplicate the slides to create a seamless loop --> */}
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
+        <div className='serviceList1 slider-container'>
+          <div className='slider1'>
+            <div className='slide' style={{backgroundColor:"#FF9344"}}>First1</div>
+            <div className='slide' >OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#FFD74B"}}>OUR SERVICES1</div>
+            <div className='slide' >OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#63D683"}}>OUR SERVICES1</div>
+            <div className='slide' >OUR SERVICES1</div>
           </div>
-          <div className='serviceFirst slider'>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            {/* <!-- Duplicate the slide className='test's to create a seamless loop --> */}
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
-            <div className='slide'><span className='test'>OUR SERVICES</span></div>
+          <div className='slider1'>
+          <div className='slide' style={{backgroundColor:"#FF9344"}}>First1</div>
+            <div className='slide' >OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#FFD74B"}}>OUR SERVICES1</div>
+            <div className='slide' >OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#63D683"}}>OUR SERVICES1</div>
+            <div className='slide' >OUR SERVICES1</div>
           </div>
-          <div className='serviceSecond slider'>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            {/* <!-- Duplicate the slides to create a seamless loop --> */}
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-            <div className='slide'><span>OUR SERVICES</span></div>
-          </div>
+          
         </div>
-    </div>
+        <div className='serviceList2 slider-container'>
+          <div className='slider2'>
+            <div className='slide' style={{backgroundColor:"#5031A9"}}>First1</div>
+            <div className='slide'>OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#FF3F84"}}>OUR SERVICES1</div>
+            <div className='slide'>OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#FF9344"}}>OUR SERVICES1</div>
+            <div className='slide'>OUR SERVICES1</div>
+          </div>
+          <div className='slider2'>
+          <div className='slide' style={{backgroundColor:"#5031A9"}}>First1</div>
+            <div className='slide'>OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#FF3F84"}}>OUR SERVICES1</div>
+            <div className='slide'>OUR SERVICES1</div>
+            <div className='slide' style={{backgroundColor:"#FF9344"}}>OUR SERVICES1</div>
+            <div className='slide'>OUR SERVICES1</div>
+          </div>
+         
+        </div>  
+       
+      </div>
     </>
   )
 }
